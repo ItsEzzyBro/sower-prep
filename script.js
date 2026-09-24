c4a3bd0c0baa1bb40b71e6166a6eab4fc7e5d4e4
Esmeralda Camille <esmeraldacamille@gmail.com>
2026-09-24 19:20:08 -0400

    Built a simple Welcome Page

---
+++ script.js
@@ -0,0 +1,5 @@
+const getStartedButton = document.getElementById("getStartedButton");
+
+getStartedButton.addEventListener("click", function () {
+    alert("Welcome to the Children's Ministry Teacher Assistant!");
+});
